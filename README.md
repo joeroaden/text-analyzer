43ae5e9178b6ab1873dc79dce65c8e5f5ee2726b
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-07 16:26:22 -0700

    update directory when committing

--- README.md
+++ README.md
@@ -74,10 +74,22 @@ Describe: numberOfOccurrencesInText()
   Expected Output: 3  
 
 
-Describe: badWordsNotGood()
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
+
+
+
+
+
+
+<!-- Describe: badWordsNotGood()
   Test: "It should omit the following bad words: zoinks, muppeteer, biffaroni, and loopdaloop"
   Code:
   const text = "zoinks, muppeteer, biffaroni, loopdaloop";
   const word = "loopdaloop", "zoinks", "muppeteer", "biffaroni";
   badWordsNotGood(word, text);
-  Expected Output: ""
+  Expected Output: "" -->
--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,20 @@ function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
   }
-  
   let wordCount = 0;
   const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
-    wordCount++;
+      wordCount++;
     }
   });
   return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if ((text.trim().length === 0) || (word.trim().length=== 0)){
     return 0;
   }
-  
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
@@ -30,18 +28,31 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
- function badWordsNotGood(word, text) {
-  let badWordArray = text.split(" ");
-   badWordArray.forEach(function(element) {
-     if (element.toLowerCase().includes(word.toLowerCase())) {
-       return badWordArray.filter(word);
-     }
-   });
-   
-}
+//  function badWordsNotGood(word, text) {
+//   let badWordArray = text.split(" ");
+//    badWordArray.forEach(function(element) {
+//      if (element.toLowerCase().includes(word.toLowerCase())) {
+//        return badWordArray.filter(word);
+//      }
+//    });
 
-const word = "zoinks";
-const text = "you little zoinks"
+// }
 
-badWordsNotGood(word, text);
+// const word = "zoinks";
+// const text = "you little zoinks"
 
+// badWordsNotGood(word, text);
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
